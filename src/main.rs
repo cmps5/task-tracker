8830978f2c83de740826eed0b3056439b63a0e12
cmps5 <cmps.5@hotmail.com>
2025-01-12 09:26:49 +0000

    add command-line interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap :: {Parser, Subcommand};
+
+// define the command line interface structure
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+// define the subcommands
+#[derive(Subcommand)]
+enum Commands {
+    Add {
+        name: String,
+    },
+    Complete {
+        name: String,
+    },
+    Status,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+    Commands :: Add {name} => {
+        println!("Adding task: {}", name);
+    } 
+     Commands :: Complete { name } => {
+        println!("Completing task: {}", name);
+     }
+     Commands :: Status => {
+        println!("Showing status for all task");
+     }     
+    }
 }
